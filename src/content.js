3cbf5d9f225eaa0665c102feb43d4108b92e6038
Trong Nguyen <daotrong2000@gmail.com>
2018-02-17 23:21:00 +0700

    nhu cc

--- src/content.js
+++ src/content.js
@@ -1,3 +1,11 @@
 export default [
-  "chán ghê chứ","haiz"
+  "M2Love",
+  "I adore you",
+  "I’m affectionate for you",
+  "I’m attached to you",
+  "I’m enchanted by you",
+  "You’re my missing piece",
+  "You’re the only one for me",
+  "I’m totally devoted to you",
+  "I’m infatuated with you"
 ];
